d5e35acce8001084606f32568d5af0b99daee7b0
Charlie <cvuong90@gmail.com>
2022-06-13 16:05:20 -0700

    Use core actions

--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
-import ghpages from 'gh-pages';
+import core from '@actions/core';
+import github from '@actions/github';
 
-(async function () {
-  function ghPagesPromise() {
-    return new Promise((resolve, reject) => {
-      ghpages.publish(
-        'static',
-        {
-          repo: 'https://github.com/cvuong/ghpages-test.git',
-          user: {
-            name: 'github-actions-bot',
-            email: 'test@test.com',
-          },
-          dest: 'blah1',
-        },
-        (err) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve();
-        }
-      );
-    });
-  }
+// import ghpages from 'gh-pages';
 
-  const res = await ghPagesPromise();
-  if (!res) {
-    console.log('Success');
-  } else {
-    console.log('Error');
-  }
+// (async function () {
+//   function ghPagesPromise() {
+//     return new Promise((resolve, reject) => {
+//       ghpages.publish(
+//         'static',
+//         {
+//           repo: 'https://github.com/cvuong/ghpages-test.git',
+//           user: {
+//             name: 'github-actions-bot',
+//             email: 'test@test.com',
+//           },
+//           dest: 'blah1',
+//         },
+//         (err) => {
+//           if (err) {
+//             reject(err);
+//             return;
+//           }
+//           resolve();
+//         }
+//       );
+//     });
+//   }
+
+//   const res = await ghPagesPromise();
+//   if (!res) {
+//     console.log('Success');
+//   } else {
+//     console.log('Error');
+//   }
+// })();
+
+(function () {
+  const token = core.getInput('myToken');
+  const octokit = github.getOctokit(token);
 })();
